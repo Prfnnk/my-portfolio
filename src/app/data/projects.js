fe875ac63e6a747fd7e0b78dc9d008c8ed8e3e08
Maria <maria@askphill.com>
2026-02-08 16:52:49 +0100

    Add a new project

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -4,10 +4,12 @@ import whatToDoSrc from '@/app/assets/images/projects/whattodo.png';
 import stoovSrc from '@/app/assets/images/projects/stoov.png';
 import InsectHeroesSrc from '@/app/assets/images/projects/insect_heroes.png';
 import popsaSrc from '@/app/assets/images/projects/popsa.png';
+import tonysSrc from '@/app/assets/images/projects/tonys.jpg';
 
 import gsapSrc from '@/app/assets/images/icons/gsap.jpeg';
 import pugSrc from '@/app/assets/images/icons/pug.svg';
 import liquidSrc from '@/app/assets/images/icons/liquid.png';
+import reactSrc from '@/app/assets/images/icons/react.svg';
 
 // icon values are from devicon library: https://devicon.dev/
 export const projects = [
@@ -36,16 +38,16 @@ export const projects = [
       link: 'https://naturella.lenta.ru/',
     },
     {
-      title: 'Stoov',
-      img: stoovSrc,
+      title: "Tony's Chocolonely",
+      img: tonysSrc,
       stack: [
         {
           name: 'Javascript',
           icon: 'javascript-plain',
         },
         {
-          name: 'Liquid',
-          icon: liquidSrc,
+          name: 'React',
+          icon: reactSrc,
         },
         {
           name: 'Tailwind',
@@ -56,7 +58,7 @@ export const projects = [
           icon: 'Shopify',
         },
       ],
-      link: 'https://nl.stoov.com/',
+      link: 'https://nl.tonyschocolonely.com/',
     },
     {
       title: 'Cancel Culture',
@@ -107,8 +109,8 @@ export const projects = [
       link: 'https://whattodo.lenta.ru/',
     },
     {
-      title: 'Insect Heroes',
-      img: InsectHeroesSrc,
+      title: 'Stoov',
+      img: stoovSrc,
       stack: [
         {
           name: 'Javascript',
@@ -119,15 +121,15 @@ export const projects = [
           icon: liquidSrc,
         },
         {
-          name: 'Sass',
-          icon: 'sass-original',
+          name: 'Tailwind',
+          icon: 'tailwindcss-plain',
         },
         {
           name: 'Shopify',
           icon: 'Shopify',
         },
       ],
-      link: 'https://insectheroes.nl/',
+      link: 'https://nl.stoov.com/',
     },
     {
       title: 'Pop Music History',
@@ -153,4 +155,52 @@ export const projects = [
       link: 'https://popsa.lenta.ru/',
     },
   ],
+  // [
+  //   {
+  //     title: 'Insect Heroes',
+  //     img: InsectHeroesSrc,
+  //     stack: [
+  //       {
+  //         name: 'Javascript',
+  //         icon: 'javascript-plain',
+  //       },
+  //       {
+  //         name: 'Liquid',
+  //         icon: liquidSrc,
+  //       },
+  //       {
+  //         name: 'Sass',
+  //         icon: 'sass-original',
+  //       },
+  //       {
+  //         name: 'Shopify',
+  //         icon: 'Shopify',
+  //       },
+  //     ],
+  //     link: 'https://insectheroes.nl/',
+  //   },
+  //   {
+  //     title: 'Marlies Dekkers',
+  //     img: InsectHeroesSrc,
+  //     stack: [
+  //       {
+  //         name: 'Javascript',
+  //         icon: 'javascript-plain',
+  //       },
+  //       {
+  //         name: 'Liquid',
+  //         icon: liquidSrc,
+  //       },
+  //       {
+  //         name: 'Sass',
+  //         icon: 'sass-original',
+  //       },
+  //       {
+  //         name: 'Shopify',
+  //         icon: 'Shopify',
+  //       },
+  //     ],
+  //     link: 'https://www.marliesdekkers.com/nl',
+  //   },
+  // ]
 ];
